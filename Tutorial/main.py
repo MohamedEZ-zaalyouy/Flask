5fd28f681ad4d0e412da6b7aa0b358175af4a904
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-03-04 20:27:09 +0100

    templets

--- Tutorial/main.py
+++ Tutorial/main.py
@@ -3,3 +3,29 @@
 # ----------------------------------------
 # - Flask Is Micro Framework Built With Python
 # --------------------------------------------
+
+
+from flask import Flask , render_template
+
+
+skills_app = Flask(__name__)
+
+
+
+@skills_app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+
+@skills_app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ == "__main__":
+    skills_app.run(debug=True, port=9000)
+
+
+
