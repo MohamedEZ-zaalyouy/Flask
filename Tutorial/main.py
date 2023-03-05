ef1749a4cbcff4d1eaa75f902ac835e6c488ae64
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-03-05 15:52:31 +0100

    fin

--- Tutorial/main.py
+++ Tutorial/main.py
@@ -10,17 +10,40 @@ from flask import Flask , render_template
 
 skills_app = Flask(__name__)
 
+my_skills = [("Html", 80), ("CSS", 75), ("Python", 95)]
 
 
 @skills_app.route("/")
 def homepage():
-    return render_template("homepage.html", title ="home Page")
+    return render_template("homepage.html", 
+                           title ="home Page",
+                           custom_css="home"
+                           )
+
+
+@skills_app.route("/skills")
+def skills():
+    return render_template("skills.html",
+                          title="My Skills",
+                          page_head="My Skills",
+                          custom_css="skills",
+                          description="This Is My Skills Page",
+                          skills=my_skills)
+
+
+@skills_app.route("/add")
+def add():
+    return render_template('add.html',
+                            title ="Add Skills",
+                            custom_css="add"
+                           )
 
 
 
 @skills_app.route("/about")
 def about():
-    return render_template('about.html', title ="About Us")
+    return render_template('about.html',
+                           title ="About Us")
 
 
 
